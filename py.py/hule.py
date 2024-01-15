1522c40b50ff1edfd54bedd23f20e4761f35658d
nhatnam201104 <nhatnam201104@gmail.com>
2024-01-15 12:06:23 +0700

    hello

--- py.py/hule.py
+++ py.py/hule.py
@@ -1,1 +1,6 @@
 print  ("hello con di cua anh");
+print  ("hello con di cua anh");
+print  ("hello con di cua anh");
+print  ("hello con di cua anh");
+print  ("hello con di cua anh");
+print  ("hello con di cua anh");
