6a3a8876c11fce7caffa7039a9e3389bf47cfde2
nhatnam201104 <nhatnam201104@gmail.com>
2024-01-16 10:50:35 +0700

    cap nhat dong code chui con cho tran nam

--- py.py/hule.py
+++
@@ -1,7 +0,0 @@
-print  ("hello con di cua anh");
-print  ("hello con di cua anh dam vl");
-print  ("hello con di cua anh");
-print  ("hello con di cua anh");
-print  ("hello con di cua anh");
-print  ("hello con di cua anh");
-print ("con di vu bu dam dang");
